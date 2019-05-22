086e393cf9b533fc7e1cfa8078612c6fc92a1e28
Elvis Begovic <e.begovic@thurnherr.ch>
2019-05-22 16:40:45 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,7 @@ import {HomeComponent} from './home.component';
 const routes: Routes = [
   // public
   {path: '',  component: HomeComponent},
-  {path: 'absolute', loadChildren: () => import('src/app/a/a.module').then(m => m.AModule)},
-  {path: 'relative', loadChildren: () => import('./a/a.module').then(m => m.AModule)}
+  {path: 'relative', loadChildren: () => import('./a/a.module.ngfactory').then(m => m.AModuleNgFactory)}
 ] as Routes;
 
 @NgModule({
