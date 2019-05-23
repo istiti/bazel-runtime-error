73718b0cddd00afe45dc840eb62de6d6a9d8541d
istiti <elvis_203@msn.com>
2019-05-23 15:42:20 +0200

    fc

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -8,13 +8,40 @@ import { Component, OnInit } from '@angular/core';
     </p>
 	
 	<ul>
-		<li><a routerLink="/absolute">lazyload absolute path 'src/app/a/a.module'</a></li>
 		<li><a routerLink="/relative">lazyload relative path 'a/a.module'</a></li>
 	</ul>
+  <hr>
+
   `,
   styles: []
 })
 export class HomeComponent implements OnInit {
+
+  nodes = [
+    {
+      id: 1,
+      name: 'root1',
+      children: [
+        { id: 2, name: 'child1' },
+        { id: 3, name: 'child2' }
+      ]
+    },
+    {
+      id: 4,
+      name: 'root2',
+      children: [
+        { id: 5, name: 'child2.1' },
+        {
+          id: 6,
+          name: 'child2.2',
+          children: [
+            { id: 7, name: 'subsub' }
+          ]
+        }
+      ]
+    }
+  ];
+  options = {};
 
   constructor() { }
 
